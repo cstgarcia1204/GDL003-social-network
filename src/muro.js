7d8ec5f33b90527ff4a256b01f3716c5321d2aa6
Cinthia <ckstgarcia@gmail.com>
2019-08-09 12:03:38 -0500

    se agrego mostrar campo de autor y fecha en los post y se valido funcion mostrar para los campos vacios

--- src/muro.js
+++ src/muro.js
@@ -48,14 +48,6 @@ firebase.auth().onAuthStateChanged(function(user){
       document.querySelector('.photo').innerHTML=`<img class="photoProfile" src="${dataPerfil.img}" alt="photo">`;
       document.querySelector('.name').innerHTML=`<h1>${dataPerfil.name}</h1>`;
       document.querySelector('.email').innerHTML=`<h1>${dataPerfil.email}</h1>`;
-      const setUidUsuario=firebase.firestore().collection('usuarios').doc (dataPerfil.id);
-      setUidUsuario.set({
-          wallUsuario: "",
-          media:"",
-          name:user.displayName,
-          email: user.email
-
-      });
   }
   });
     
@@ -74,25 +66,23 @@ const inputText=document.querySelector('#latest');
 const buttonSave=document.querySelector('#saveButton');
 const buttonLoad=document.querySelector('#loadButton');
 const outputComentarios=document.querySelector('#coments');
-//const user=firebase.auth().currentUser;
-//const uid=user.uid;
 
-var newPostKey= firebase.database().ref().child('post').push().key;
+//para generar la key en cada post
+let newPostKey= firebase.database().ref().child('post').push().key;
 
 buttonSave.addEventListener('click',function(){
     const textToSave=inputText.value;
     console.log('Im going to save '+textToSave+' to Firestore');
 
     
-    
     const docReference1=firestore.doc('/post/' + newPostKey);  
-
-    //var updates={};
-    //updates['/post/' + newPostKey]= dataPerfil;  
         
     docReference1.set({    
         texto: textToSave,
-        autorid: firebase.auth().currentUser.uid   
+        autorid: firebase.auth().currentUser.uid,
+        nombre:firebase.auth().currentUser.displayName,
+        timeStamp: firebase.firestore.FieldValue.serverTimestamp(),
+        keyDePost:newPostKey
 
     })    .then(function(){
         console.log('Post Guardado!!');
@@ -102,20 +92,7 @@ buttonSave.addEventListener('click',function(){
     })
 });
 
-/*function obtenerDatosEnTiempoReal(){
-    var newPostKey1= firebase.database().ref().child('post').push().key;
-    const docReference2=firestore.doc('/post/' + newPostKey1); 
-
-    docReference2.onSnapshot(function(doc){
-      if(doc && doc.exists){
-          const myData= doc.data();
-          console.log('Verificando la data que estoy recibiendo: ',doc);
-          outputH1.innerHTML='Mi post es: '+ myData.post;
-      };
-
-    })
-}*/
-    
+//funcion imprimir el mensaje actual
 function postEnTiempoReal(){
     firestore.collection('post').onSnapshot(snapshot =>{ 
        let changes= snapshot.docChanges();
@@ -123,32 +100,40 @@ function postEnTiempoReal(){
         //console.log(change.doc.data());
         changes.forEach(change=>{    
         console.log(change.doc.data());
-        outputH1.innerHTML=change.doc.data().texto;
+        if(inputText.value != '' || inputText.value != null){
+            outputH1.innerHTML=`<br><p class='cajaActual'>${change.doc.data().texto+'<br>'+
+            ' Post by '+change.doc.data().nombre+'<br>'+' At '+change.doc.data().timeStamp.toDate()}</p>`;
+            console.log(change.doc.data().nombre+change.doc.data().timeStamp.toDate());
+        }else{
+            outputH1.innerHTML='No has publicado nada aún';
+        }
+        
         })
     })
  };
 postEnTiempoReal();
 
 
-//funcion todos los comentarios
+//funcion imprimir todos los comentarios
 
 function postComentarios(){
     firestore.collection('post').onSnapshot(snapshot =>{ 
        let changes= snapshot.docChanges();
         //console.log(changes);
         //console.log(change.doc.data());
-        changes.forEach(change=>{    
-        console.log(change.doc.data().texto);
-        outputComentarios.innerHTML+=`<br><p class= "caja">${change.doc.data().texto}</p><br>`;
+        changes.forEach(change=>{
+            console.log(change.doc.data().texto);
+            if(change.doc.data().texto == '' || change.doc.data().timeStamp == null){
+                console.log('Alert!!! ---> Campo vacío');
+            }else{outputComentarios.innerHTML+=`<br><p class= "caja">${change.doc.data().texto+'<br>'+
+            ' Post by '+change.doc.data().nombre+'<br>'+' At '+change.doc.data().timeStamp.toDate()+'<br>'
+            }</p><br>`}
         })
     })
  };
 postComentarios();
 
-
-//--------------------------------------------------------------------------
 //funcion para eliminar mensajes
-
 
 let eliminar = document.getElementById("eliminar");
 eliminar.addEventListener('click',function(){
@@ -163,7 +148,8 @@ eliminar.addEventListener('click',function(){
     })
  });
 
- 
+
+
 
 
 //para boton logOut devuelva a index.html
